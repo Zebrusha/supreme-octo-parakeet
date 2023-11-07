73b5a35b6b97332d9e8adeb869f0dc72aa103257
Anastasia Voronchikhina <zebrushaa@mail.ru>
2023-11-07 15:27:40 +0300

    add folder

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class BonusServiceTest {
+    @Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount;
+        amount = 1000000;
+        boolean registered = true;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void limitOverBonusforRegistered() {
+        BonusService service = new BonusService();
+
+        long amount = 100;
+        boolean registered = true;
+        long expected = 3;
+
+        long actual = service.calculate(amount, registered);
+
+    }
+
+    @Test
+    void limitOverBonusforNoneRegistered() {
+        BonusService service = new BonusService();
+
+        long amount = 100;
+        boolean registered = false;
+        long expected = 3;
+
+        long actual = service.calculate(amount, registered);
+
+    }
+}
